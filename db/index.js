26490c07c5104240efcb6e1c922cb905b940fd55
Costache Robert Valentin <costache.robert.valentin@gmail.com>
2022-11-25 04:04:38 +0100

    up

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 
 // DataBase Connected
- const dataBase = 'mongodb+srv://donrob3r7:donrob3r7@nodetuts.qjfzzzs.mongodb.net/?retryWrites=true&w=majority';
+ const dataBase = process.env.MONGODB_URI;
 mongoose.connect(dataBase, { useNewUrlParser: true, useUnifiedTopology: true }).then(
     (result) => console.log('DataBase: Connected')
 ).catch((error)=> console.log(error));
